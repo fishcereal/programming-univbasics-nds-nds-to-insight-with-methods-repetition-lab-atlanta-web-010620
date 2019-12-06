9c33458d4cd9af5497e9ad298e07ad224593092c
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-06 18:13:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def directors_totals(source)
     result[director[:name]] = gross_for_director(director)
     director_index += 1
   end
-  p "this is the result #{result}"
+  # p "this is the result #{result}"
   result
 end
 
